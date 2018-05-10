bd895a878bb7d0dfac079a4e0874b8959be801f4
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-10 13:02:27 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash =
@@ -125,4 +126,5 @@ def adding_to_dennis
      }
 	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 	programmer_hash
+	binding.pry
 end
